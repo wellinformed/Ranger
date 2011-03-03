35cedc81e1f8d72d03bd7c276ebc65b3840d7954
Jay Adkisson <jjmadkisson@gmail.com>
2011-03-03 10:09:11 -0800

    Calculation wraps IPLogic::CIDR

--- app/models/calculation.rb
+++ app/models/calculation.rb
@@ -4,27 +4,25 @@ class Calculation
   include ActiveModel::Validations
   include ActiveModel::Conversion
   extend ActiveModel::Naming
+
+  include IPLogic
   
   attr_accessor :cidr_notation_range
   
   validates_presence_of :cidr_notation_range
-  validates_format_of :cidr_notation_range, :with => /^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}\/\d{1,2}$/i
+  validates_presence_of :cidr
   
   def initialize(attributes = {})
     attributes.each do |name, value|
       send("#{name}=", value)
     end
   end
+
+  def cidr
+    @cidr ||= (CIDR(cidr_notation_range) rescue nil)
+  end
   
   def persisted?
     false
-  end
-  
-  def start_of_range
-    IPAddr.new(self.cidr_notation_range).to_range.first
-  end
-
-  def end_of_range
-    IPAddr.new(self.cidr_notation_range).to_range.last
   end
 end
