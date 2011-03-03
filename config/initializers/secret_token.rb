a4815d5ef8daeb4e79ac6f2712e37526424508c9
tim harding <tim.harding@gmail.com>
2011-03-03 12:18:25 +0000

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ranger::Application.config.secret_token = '8a4390e424daa9eb02a1738c66ee9a319eebed5a129d48e92aaf844ce33c104faabf62e4987d1034d3e4b4ea35fd0bb036c9004e8e1dea8af408ab4736c5ed0c'
