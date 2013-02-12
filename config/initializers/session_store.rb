a6d243f92837ec6d37854e9292b16cfe5eba8e28
Tim Harding <tim.harding@gmail.com>
2013-02-12 12:08:40 +0000

    fixes syntax bug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Ranger::Application.config.session_store :cookie_store, key => '_ranger_session'
+Ranger::Application.config.session_store :cookie_store, :key => '_ranger_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
