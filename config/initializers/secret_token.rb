90d1a707f3fd68488f7ec2099149c456bd9fc0fa
Tim Harding <tim.harding@gmail.com>
2013-02-12 11:52:44 +0000

    Upgrades to Rails 3.2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ranger::Application.config.secret_token = '8a4390e424daa9eb02a1738c66ee9a319eebed5a129d48e92aaf844ce33c104faabf62e4987d1034d3e4b4ea35fd0bb036c9004e8e1dea8af408ab4736c5ed0c'
+Ranger::Application.config.secret_token = '987e195bc89f5b91c1dfa7ef90cb82c13cb6ad64493f73ad74cefd87f12dde77520508aaa89bb5af26ac30f96eb71bd9b8f3e6416b41ef66726976443637cd77'
