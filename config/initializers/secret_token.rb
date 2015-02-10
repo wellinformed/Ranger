597ebbe80a3d574438bc0d0c851d84fc49342bcc
Tim Harding <tim.harding@gmail.com>
2015-02-10 15:47:28 +0000

    Rails 4.2

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Ranger::Application.config.secret_token = '987e195bc89f5b91c1dfa7ef90cb82c13cb6ad64493f73ad74cefd87f12dde77520508aaa89bb5af26ac30f96eb71bd9b8f3e6416b41ef66726976443637cd77'
--- test/functional/calculations_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CalculationsControllerTest < ActionController::TestCase
-end
